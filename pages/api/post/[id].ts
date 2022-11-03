4637b461ccf787797e6ded06584a1c4028920931
Rikirk <ricardo.s.alfaro@gmail.com>
2022-11-02 19:45:02 -0600

    Fix delete post bug

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,7 +1,7 @@
 import prisma from '../../../lib/prisma'
 
 export default async function handle(req, res) {
-    const postId = req.prisma.id;
+    const postId = req.query.id;
     if (req.method === 'DELETE') {
         const post = await prisma.post.delete({
             where: { id: postId }
